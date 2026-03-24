87eaa63578b3c99dfdc163465cd01d27dc823529
Pereira <mpereirafernandez@danielcastelao.org>
2026-03-24 13:45:41 +0100

    Comentada clase Nave

--- nave.py
+++ nave.py
@@ -1,13 +1,13 @@
 class Nave:
-    def __init__(self, nombre, tipo, vida):
+    def __init__(self, nombre, tipo, vida):#Atributos a las naves para que se identifique a cada nave con su nombre, tipo y vida.
         self.nombre=nombre
         self.tipo=tipo
-        self.vida=vida
+        self.vida=vida#La vida de la nave dependerá unicamente de su tipo.
 
-    def recibir_disparo(self):
+    def recibir_disparo(self):#La funcion recibir disparo descontará una vida a la nave
         self.vida = self.vida-1
-        print("Tocado")
-        if self.vida==0:
+        print("Tocado")#Al recibir un disparo el juego recibirá un tocado ya que se tocó una nave.
+        if self.vida==0:#Este caso para cuando una nave se queda sin vidas se hundirá por lo que esa nave deja se existir y se printeará hundido.
             print("Hundido")
 
 
