58197756b0cd31b637916427814f1b63b09b2672
MartinPereiraFernandez <mpereirafernandez@danielcastelao.org>
2026-03-24 14:11:08 +0100

    Examen 24 Marzo acabado
    * Commit para push
    
    * Estudiar en casa
    
    * Comentada clase Nave
    
    * Comentada clase Casilla
    
    * Comentada clase Tablero
    
    * Comentada clase Juego
    
    * Acabado readme y examen.

--- nave.py
+++ nave.py
@@ -1,16 +1,14 @@
-from juego import Juego
-from tablero import Tablero
 class Nave:
-    def __init__(self, nombre,tipo,vida):
+    def __init__(self, nombre, tipo, vida):#Atributos a las naves para que se identifique a cada nave con su nombre, tipo y vida.
         self.nombre=nombre
         self.tipo=tipo
-        self.vida=vida
+        self.vida=vida#La vida de la nave dependerá unicamente de su tipo.
 
-    def recibirDisparo(self):
+    def recibir_disparo(self):#La funcion recibir disparo descontará una vida a la nave
         self.vida = self.vida-1
-
-
-
+        print("Tocado")#Al recibir un disparo el juego recibirá un tocado ya que se tocó una nave.
+        if self.vida==0:#Este caso para cuando una nave se queda sin vidas se hundirá por lo que esa nave deja se existir y se printeará hundido.
+            print("Hundido")
 
 
 
--- tablero.py
+++ tablero.py
@@ -1,13 +1,91 @@
+
+from casilla import Casilla
+from nave import Nave
+
 class Tablero:
 
-    def __init__(self, tamano=10):
-        self.AGUA = 0
-        self.TOCADO = 1
-        self.HUNDIDO = 2
+    def __init__(self, tamano=10): #Este init le da numeros a las respuestas para que retorne agua, tocado o hundido.
+        self.agua = 0
+        self.tocado = 1
+        self.hundido = 2
 
-    def colocar_nave(self, nave, x, y, orientacion):
+        por1=Nave("Enterprise","portaaviones",5) #Se les da forma a todas las naves creadas para luego meterlas dentro del tablero.
+
+        fra1=Nave("Bismarck","fragata",3)
+        fra2=Nave("Prince of Wales","fragata",3)
+        fra3=Nave("Graf Spee","fragata",3)
+
+        sub1 = Nave("U-47", "submarino", 1)
+        sub2 = Nave("U-96", "submarino", 1)
+        sub3 = Nave("U-505", "submarino", 1)
+        sub4 = Nave("U-534", "submarino", 1)
+
+        print(por1.nombre)
+
+        self.casillero = [
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()],
+
+            [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(),
+             Casilla()]
+        ]
+        #Todas estas funciones repetitivas asignan los barcos a sus respectivas casillas.
+        self.casillero[1][1].nave = por1
+        self.casillero[1][2].nave = por1
+        self.casillero[1][3].nave = por1
+        self.casillero[1][4].nave = por1
+        self.casillero[1][5].nave = por1
+
+
+        self.casillero[3][3].nave = fra1
+        self.casillero[4][3].nave = fra1
+        self.casillero[5][3].nave = fra1
+
+        self.casillero[7][1].nave = fra2
+        self.casillero[7][2].nave = fra2
+        self.casillero[7][3].nave = fra2
+
+        self.casillero[9][1].nave = fra3
+        self.casillero[9][2].nave = fra3
+        self.casillero[9][3].nave = fra3
+
+
+        self.casillero[4][6].nave = sub1
+        self.casillero[9][9].nave = sub2
+        self.casillero[7][6].nave = sub3
+        self.casillero[9][5].nave = sub4
+
+    def colocar_nave(self,nave,x,y,orientacion): #Funcion creada pero que aun no está funcional, tienen que darme mas presupuesto para que la haga.
         pass
 
-    def comprobar_impacto(self, x, y):
-        print("[LOG] estoy en tablero comprobando impacto")
-        return self.AGUA
+    def comprobar_impacto(self,x,y):
+        print(f"[LOG] Comprobando impacto ({x},{y})") #Printea la casilla que has marcado.
+        return self.casillero[x][y].disparar()
+
+
+
+
