ade32c5a7d5fb68eda202e81088ac4b0bfce3586
Pereira <mpereirafernandez@danielcastelao.org>
2026-03-24 14:01:09 +0100

    Comentada clase Juego

--- juego.py
+++ juego.py
@@ -7,17 +7,17 @@ from tablero import Tablero
 class Juego:
     def __init__(self):
         self.obj_tablero = Tablero()
-        self.lanzar_ataque(1, 1)
-        self.lanzar_ataque(1, 1)
-        self.lanzar_ataque(1, 1)
-        self.lanzar_ataque(1, 1)
-        self.lanzar_ataque(1, 1)
-        self.lanzar_ataque(0,0)
+        self.lanzar_ataque(1, 1) #Con esto el usuario manda un ataque a la cordenada 1/1 en este caso hay una nave, por lo que se retornara tocado.
+        self.lanzar_ataque(1,2)
+        self.lanzar_ataque(1, 3)
+        self.lanzar_ataque(1, 4)
+        self.lanzar_ataque(1, 5)
 
-    def inicializar_naves(self):
+
+    def inicializar_naves(self): #Otra función sin realizar, de verdad, debería cobrar más.
         pass
 
-    def mostrar_resultado(self, resultado: int):
+    def mostrar_resultado(self, resultado: int): #Los numeros asignados anteriormente permiten que ahora esas variables se intercambien por un printeo.
         if resultado == 0:
             print("Agua")
         elif resultado == 1:
@@ -25,7 +25,7 @@ class Juego:
         elif resultado == 2:
             print("Hundido")
 
-    def lanzar_ataque(self, x, y):
+    def lanzar_ataque(self, x, y): #El usuario lanza el ataque
         print(f"Atacando a  {x}, {y} ")
         resultado = self.obj_tablero.comprobar_impacto(x, y)
         self.mostrar_resultado(resultado)
